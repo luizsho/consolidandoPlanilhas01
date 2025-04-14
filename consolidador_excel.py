44d27149365a5e6dca3bde3651626180ba71d2b6
Luiz Fernando <luizsho@gmail.com>
2025-04-14 12:44:37 -0300

    Projeto da EmpowerData consolidando planilhas do Excel em Phyton

---
+++ consolidador_excel.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import os
+import datetime
+
+data = datetime.datetime.now()
+
+#Criando um data frame vazio com a estrutura final do consolidado
+colunas = [
+    'Segmento',
+    'País',
+    'Produto',
+    'Qtde de Unidades Vendidas',
+	'Preço Unitário',
+    'Valor Total',
+    'Desconto',
+    'Valor Total c/ Desconto',
+    'Custo Total',
+    'Lucro',
+    'Data',
+    'Mês',
+    'Ano'
+
+]
+consolidado =  pd.DataFrame(columns=colunas)
+
+#Busca o nome dos arquivos a serem consolidados
+arquivos = os.listdir("planilhas")
+
+#Realiza a consolidação apenas dos arquivos .xlsx
+for excel in arquivos:
+
+    if excel.endswith(".xlsx"):
+        dados_arquivo =  excel.split("-")
+        segmento = dados_arquivo[0]
+        pais =  dados_arquivo[1].replace(".xlsx", "")
+
+        try:
+            df = pd.read_excel(f'planilhas\\{excel}')
+            df.insert(0, 'Segmento', segmento)
+            df.insert(1, 'País', pais)
+            consolidado = pd.concat([consolidado, df])
+        except:
+            with open('log_erros.txt', 'a') as arquivo:
+                arquivo.write(f'Erro ao tentar consolidar o arquivo {excel}.')
+    else:
+        with open('log_erros.txt', 'a') as arquivo:
+                    arquivo.write(f'O arquivo {excel} não é um arquivo excel válido!')
+
+# exporta o data frame consolidado para um arquivo excel
+consolidado.to_excel(f"Report-consolidado-{data.strftime('%d-%m-%Y')}.xlsx", 
+                     index=False,
+                     sheet_name='Report consolidado')
